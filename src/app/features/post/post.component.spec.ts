cb198b9ba2ed71fb70efd628607a8513dcc9267c
BluubiDev <bluubi.dev@gmail.com>
2023-08-04 19:48:03 +0200

    Añadido tests resueltos de tutorial básico sobre testing de componentes con inputs

--- src/app/features/post/post.component.spec.ts
+++ src/app/features/post/post.component.spec.ts
@@ -1,6 +1,6 @@
 import {PostComponent} from './post.component';
 import {TestBed} from "@angular/core/testing";
-import {CommonModule} from "@angular/common";
+import {BoardComponent} from "../board/board.component";
 
 describe('PostComponent', () => {
   it('should render posts', () => {
@@ -15,42 +15,15 @@ describe('PostComponent', () => {
     expect(content.textContent).toContain('Crear el primer test de integración entre dos componentes de Angular');
 
   });
-
-  it('should render all posts', () => {
-    const { fixture, component } = sut();
-
-    component.posts = [{
-      title: 'Awesome title',
-      content: 'Awesome content'
-    },
-      {
-        title: 'Another awesome title',
-      content: 'Another awesome content'
-      }];
-
-
-    fixture.detectChanges();
-
-    const title = fixture.nativeElement.querySelectorAll('.title') as HTMLHeadElement[];
-    const content = fixture.nativeElement.querySelectorAll('.content') as HTMLParagraphElement[];
-
-    expect(title.length).toBe(2)
-    expect(content.length).toBe(2)
-
-     expect(title[0].textContent).toContain('Awesome title');
-     expect(title[1].textContent).toContain('Another awesome title');
-     expect(content[0].textContent).toContain('Awesome content');
-     expect(content[1].textContent).toContain('Another awesome content');
-  })
 });
 
 
 function sut() {
   TestBed.configureTestingModule({
-    imports: [PostComponent]
+    imports: [BoardComponent, PostComponent]
   });
 
-  const fixture = TestBed.createComponent(PostComponent);
+  const fixture = TestBed.createComponent(BoardComponent);
 
   const component = fixture.componentInstance;
 
