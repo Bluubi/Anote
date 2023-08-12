d0a1e9cad20b460336fe7f12fda128b73845f990
BluubiDev <bluubi.dev@gmail.com>
2023-08-13 00:41:38 +0200

    Tests resueltos

--- src/app/features/board/board.component.spec.ts
+++ src/app/features/board/board.component.spec.ts
@@ -1,23 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { BoardComponent } from './board.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {BoardComponent} from './board.component';
+import {PostComponent} from "../post/post.component";
+import {FormComponent} from "../post/features/form/form.component";
+import {AppRoutingModule} from "../../app-routing.module";
+import {RouterTestingHarness} from "@angular/router/testing";
+import {Router} from "@angular/router";
 
 describe('BoardComponent', () => {
-  let component: BoardComponent;
-  let fixture: ComponentFixture<BoardComponent>;
+  it('should navigate to concrete post when user click on it', async () => {
+    const { fixture, router } = await setup();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BoardComponent ]
-    })
-    .compileComponents();
+    const post = fixture.nativeElement.querySelector('app-post') as HTMLElement;
 
-    fixture = TestBed.createComponent(BoardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    const routerDI = TestBed.inject(Router);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
+    const click = new MouseEvent('click');
+    post.dispatchEvent(click);
+
+    const form = router.nativeElement.querySelector('app-form');
+
+    expect(form).toBeDefined();
+    expect(routerDI.url).toEqual('/post/0')
+  })
 });
+
+async function setup() {
+
+  await TestBed.configureTestingModule({
+    imports: [AppRoutingModule, PostComponent, FormComponent],
+  }).compileComponents();
+
+  const routerFixture = await RouterTestingHarness.create();
+
+  const router = routerFixture.fixture as ComponentFixture<unknown>;
+
+  const fixture = TestBed.createComponent(BoardComponent);
+  const sut = fixture.componentInstance;
+
+  fixture.autoDetectChanges(true);
+
+  return { fixture, sut, router }
+}
